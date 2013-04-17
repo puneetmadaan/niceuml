c186a095c1cab082a50a83953385d9e3641e0480
Jakub Červenka <redwormik@gmail.com>
2013-04-17 08:11:22 +0200

    Routes extension instead factory

---
+++ app/router/RoutesExtension.php
@@ -0,0 +1,62 @@
+<?php
+
+use Nette\Config\Compiler;
+use Nette\Config\CompilerExtension;
+use Nette\Utils\Validators;
+use Nette\Application\Routers\Route;
+use Nette\Utils\Arrays;
+
+
+class RoutesExtension extends CompilerExtension {
+
+	public $routeDefaults = array(
+		'mask' => NULL,
+		'action' => array(),
+		'oneWay' => FALSE,
+		'secured' => FALSE,
+		'caseSensitive' => FALSE,
+	);
+
+	public function loadConfiguration() {
+		$container = $this->getContainerBuilder();
+		$config = $this->getConfig();
+
+		$router = $container->addDefinition($this->prefix('router'))
+			->setClass('Nette\Application\Routers\RouteList');
+
+		foreach ($config as $key => $value) {
+			if ($value instanceof \stdClass) {
+				$value = Compiler::filterArguments(array($value));
+				$router->addSetup('$route = ?; if ($route instanceof IRouterFactory) $route = $route->createRouter(); $service[] = $route', $value);
+			}
+			else {
+				$value = (array) $value;
+				if (!Validators::isNumeric($key))
+					array_unshift($value, $key);
+
+				if (!isset($value['mask']) && isset($value[0]))
+					$value['mask'] = $value[0];
+				elseif (!isset($value['action']) && isset($value[0]))
+					$value['action'] = $value[0];
+				if (!isset($value['action']) && isset($value[1]))
+					$value['action'] = $value[1];
+
+				$value = $value + $this->routeDefaults;
+
+				$flags = 0;
+				if ($value['oneWay'] || in_array('oneWay', $value, TRUE))
+					$flags |= Route::ONE_WAY;
+				if ($value['secured'] || in_array('secured', $value, TRUE))
+					$flags |= Route::SECURED;
+				if ($value['caseSensitive'] || in_array('caseSensitive', $value, TRUE))
+					$flags |= Route::CASE_SENSITIVE;
+
+				$router->addSetup(
+					'$service[] = new Nette\Application\Routers\Route(?,?,?)',
+					array($value['mask'], $value['action'], $flags)
+				);
+			}
+		}
+	}
+
+}
