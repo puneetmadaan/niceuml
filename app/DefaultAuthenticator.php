240766052b5f95553a41dedd7a2c50fc5b87e6e1
Jakub Červenka <redwormik@gmail.com>
2013-04-18 04:41:27 +0200

    Adding DefaultAuths

---
+++ app/DefaultAuthenticator.php
@@ -0,0 +1,14 @@
+<?php
+
+use Nette\Security\IAuthenticator;
+use Nette\Security\AuthenticationException;
+
+
+class DefaultAuthenticator extends Nette\Object implements IAuthenticator {
+	
+	public function authenticate(array $credentials) {
+		list($username) = $credentials;
+		throw new AuthenticationException("User '$username' not found.", self::IDENTITY_NOT_FOUND);
+	}
+
+}
---
+++ app/DefaultAuthorizator.php
@@ -0,0 +1,12 @@
+<?php
+
+use Nette\Security\IAuthorizator;
+
+
+class DefaultAuthorizator extends Nette\Object implements IAuthorizator {
+	
+	function isAllowed($role, $resource, $privilege) {
+		return TRUE;
+	}
+
+}
