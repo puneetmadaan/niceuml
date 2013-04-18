03c6e1ed1272813ca5d0903d0f442a014a6e3a3d
Jakub Červenka <redwormik@gmail.com>
2013-04-18 04:37:15 +0200

    Adding MenuControl

---
+++ app/components/MenuControl.php
@@ -0,0 +1,50 @@
+<?php
+
+use Nette\Security\User;
+use Nette\Utils\Arrays;
+
+class MenuControl extends BaseControl {
+
+	protected $links = array();
+
+	/** @var Nette\Security\User */
+	protected $user;
+
+	public function __construct(array $links, User $user) {
+		$this->links = $links;
+		$this->user = $user;
+	}
+	
+
+	public function render() {
+		$links = array();
+		foreach ($this->links as $l) {
+			$link = array(
+				'link' => Arrays::get($l, 'link', ''),
+				'args' => Arrays::get($l, 'args', array()),
+				'label' => Arrays::get($l, 'label', ''),
+			);
+			if (!array_key_exists('access', $l))
+				$links[] = $link;
+			else {
+				if ($l['access'] instanceof Nette\Callback)
+					$allowed = $l['access']->invoke();
+				else {
+					if (is_array($l['access']))
+						list($resource, $privilege) = $l['access'];
+					else
+						list($resource, $privilege) = array($l['access'], NULL);
+					if ($resource === '') $resource = NULL;
+					$allowed = $this->user->isAllowed($resource, $privilege);
+				}
+				if ($allowed xor !empty($l['accessInverse']))
+					$links[] = $link;
+			}
+		}
+		$this->template->links = $links;
+		parent::render();
+	}
+
+
+
+}
