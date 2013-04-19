bd838acff2a40bf71569cab3023ab62871322460
Jakub Červenka <redwormik@gmail.com>
2013-04-19 12:00:35 +0200

    First element implementation

--- app/presenters/ElementPresenter.php
+++ app/presenters/ElementPresenter.php
@@ -15,7 +15,8 @@ class ElementPresenter extends ModellingPresenter {
 	
 
 	public function actionEdit($id) {
-
+		$row = $this->project->related('core_element')->get($id);
+		$this->element = $row->related($row->type)->fetch();
 	}
 
 	public function renderEdit() {
@@ -23,13 +24,59 @@ class ElementPresenter extends ModellingPresenter {
 	}
 
 	public function createComponentNewElementForm() {
-		return $this->context->createNewElementForm();
+		$form = $this->createForm();
+		$items = array(
+			'note' => 'Note',
+			'class' => 'Class',
+		);
+		$form->addSelect('type', 'Type', $items)
+			->setRequired();
+		$form->addSubmit('send');
+		$form->onSuccess[] = $this->newElementFormSucceeded;
+		return $form;
+	}
+
+	public function newElementFormSucceeded($form) {
+		$values = $form->values;
+		$row = $this->project->related('core_element')->insert($values);
+		if ($values->type === 'note')
+			$row->related('core_note')->insert(NULL);
+		elseif ($values->type === 'class')
+			$row->related('class_class')->insert(NULL);
+		$this->redirect('edit', $row->id);
 	}
 
 	public function createComponentElementControl() {
+		$form = $this->createForm();
+		$form->addTextarea('source','Source');
+			//->defaultValue = Nette\Utils\Neon::encode( $this->element->toArray() + $this->element->ref('core_element', 'id')->toArray() );
+
+		$form->addSubmit('send');
+		$form->onSuccess[] = $this->elementControlSucceeded;
+		
+		return $form;
+		/*
 		if (!$this->element)
 			return NULL;
 		return $this->context->createElementControl($this->element->type);
+		*/
+	}
+
+	public function elementControlSucceeded($form) {
+		try {
+			$source = Nette\Utils\Neon::decode( $form['source']->value );
+		} catch ( Nette\Utils\NeonException $e ) {
+			$form['source']->addError($e->getMessage());
+			return;
+		}
+		if (isset($source['name'])) {
+			$this->element->ref('core_element', 'id')->update(array(
+				'name' => $source['name'],
+			));
+			unset($source['name']);
+		}
+		$this->element->update($source);
+		$this->redirect('this');
 	}
 
 	public function createComponentNewRelationForm() {
