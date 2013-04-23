e7b3329b513d2f51fd7f101b39d403375030223e
Jakub Červenka <redwormik@gmail.com>
2013-04-23 20:18:30 +0200

    Model events

--- app/model/Base.php
+++ app/model/Base.php
@@ -2,12 +2,46 @@
 
 namespace Model;
 
-use NiceDAO\Service;
-use Nette\Security\User;
-use NiceDAO\Selection;
+use NiceDAO\Entity,
+	NiceDAO\Selection,
+	NiceDAO\Service,
+	Nette\Security\User;
 
 
 class Base extends Service {
+
+	/** @var array of function(Entity $entity) */
+	public $onSave = array();
+
+	/** @var array of function(Entity $entity) */
+	public $onCreate = array();
+
+	/** @var array of function(Entity $entity) */
+	public $onUpdate = array();
+
+	/** @var array of function(Entity $entity) */
+	public $onDelete = array();
+
+
+	public function save(Entity $entity = NULL, $data = NULL) {
+		$new = $entity === NULL || $this->isNew($entity);
+		$entity = parent::save($entity, $data);
+
+		if ($new)
+			$this->onCreate($entity);
+		else
+			$this->onUpdate($entity);
+
+		$this->onSave($entity);
+		return $entity;
+	}
+
+
+	public function delete(Entity $entity) {
+		$return = parent::delete($entity);
+		$this->onDelete($entity);
+		return $return;
+	}
 
 
 	public function filterAllowed(User $user, Selection $table, $action = NULL) {
