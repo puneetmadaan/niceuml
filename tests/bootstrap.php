4cdc7b5035694eae9e18bb3321b49aff311fb547
Jakub Červenka <redwormik@gmail.com>
2013-05-08 13:42:37 +0200

    Removed unused tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,38 @@
 <?php
 
-require __DIR__ . '/../libs/autoload.php';
-
-if (!include __DIR__ . '/../libs/nette/tester/Tester/bootstrap.php') {
-	die('Install Nette Tester using `composer update --dev`');
+if (@!(include __DIR__ . '/../libs/autoload.php') || @!(include __DIR__ . '/../libs/nette/tester/Tester/bootstrap.php')) {
+	echo 'Install Nette Tester using `composer update --dev`';
+	exit(1);
 }
+
+
+// configure environment
+Tester\Helpers::setup();
+date_default_timezone_set('Europe/Prague');
+
 
 function id($val) {
 	return $val;
 }
 
+
+function createTempDir() {
+	$dir = __DIR__ . '/../temp/tests/' . getmypid();
+	@mkdir(dirname($dir));
+	Tester\Helpers::purge($dir);
+	return $dir;
+}
+
+
 $configurator = new Nette\Config\Configurator;
 $configurator->setDebugMode(FALSE);
 $configurator->setTempDirectory(__DIR__ . '/../temp');
+
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__ . '/../app')
 	->register();
 
-$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
+$configurator->addConfig(__DIR__ . '/../app/config/config.neon', $configurator::NONE);
 $configurator->addConfig(__DIR__ . '/config.neon', $configurator::NONE); // none section
 
 $configurator->onCompile[] = function ($configurator, $compiler) {
